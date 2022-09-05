2bf39f43a57b0475a9c28ec08c1f4b436cbbced5
jerindm <112775852+jerindm@users.noreply.github.com>
2022-09-06 00:08:18 +0530

    Add files via upload

---
+++ day1Class/OddOrEven.java
@@ -0,0 +1,15 @@
+package mavenproject;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i =15;
+if (i%2==0) {
+	System.out.println("even");
+}
+	else {
+		System.out.println("odd");
+	}
+}
+	}
---
+++ day1Class/SeptemberDay1.java
@@ -0,0 +1,12 @@
+package mavenproject;
+
+public class SeptemberDay1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to day 1");
+		System.out.println("welcome to testleaf");
+
+	}
+
+}
